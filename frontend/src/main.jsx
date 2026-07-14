614b3adf49195d9abcddf1e3d60f2771a5e2a51f
unknown <eirricsanchez@gmail.com>
2026-07-14 11:34:20 -0500

    Mejora: prepara estructuración y optimización de componentes React

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
+  // TODO: Habilitar React.StrictMode para auditar componentes legacy
   <StrictMode>
     <App />
   </StrictMode>,
